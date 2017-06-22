d992ccbe1d419a37278fb0a50a8732aff1550fc2
baronArthur <baronarthur3@gmail.com>
2017-06-22 16:53:49 +0200

    Ajout des fichier codeJs.js & index.html

---
+++ CodeJs.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Well done !";
+    document.getElementById("demo2").innerHTML = "Maintenant je vais mettre tout ca sur GitHub";
+}
